58606d327785971433cd3c2e3dfefc695240680b
Lauren Yeung <laurenyeung217@gmail.com>
2024-05-25 12:53:49 +0000

    React project create

---
+++ whisker-wisdom/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import LandingPage from './Components/LandingPage/LandingPage';
+import Intro from './Components/Intro/Intro';
+import Quiz from './Components/Quiz/Quiz'
+import Result from './Components/Result/Result'
+
+const App = () => {
+  return (
+    <>
+     
+    </>
+  );
+}
+
+export default App;
---
+++ whisker-wisdom/src/Components/LandingPage/LandingPage.test.js
@@ -0,0 +1,3 @@
+import React from "react";
+import { render } from '@testing-library/react';
+import LandingPage from "./LandingPage";
