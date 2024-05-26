2460fb8e0a534f976f8ec7c8e4f37b2b1646ed66
Lauren Yeung <laurenyeung217@gmail.com>
2024-05-26 23:00:42 +0000

    Implement quiz functionally and tests, including display the question and answer of the quiz, add function to checking answer

--- whisker-wisdom/src/Components/Quiz/Quiz.js
+++ whisker-wisdom/src/Components/Quiz/Quiz.js
@@ -1,33 +1,62 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Quiz.css'
-import catBg3 from '../../assets/images/cat5.png';
 import catBg4 from '../../assets/images/cat4.png';
+import { data } from '../../assets/JS/data'
 
 const Quiz = () => {
+
+  const [questionIndex, setQuestionIndex] = useState(0);
+  const [question, setQuestion] = useState(data[questionIndex]);
+  const [lock, setLock] = useState(false)
+
+  let option1 = useRef(null);
+  let option2 = useRef(null);
+  let option3 = useRef(null);
+  let option4 = useRef(null);
+
+  let option_arr = [option1, option2, option3, option4]
+
+  useEffect(() => {
+    setQuestion(data[questionIndex]);
+  }, [questionIndex]);
+
+  const checkAnswer = (option, ans) => {
+    if (!lock) {
+      if (question.ans === ans) {
+        option.target.classList.add("bg-success");
+        setLock(true);
+      } else {
+        option.target.classList.add("bg-danger");
+        setLock(true);
+        option_arr[question.ans-1].current.classList.add('bg-success')
+      }
+    } 
+  };
+
   return (
     <div className='container quiz-container m-auto'>
 
       <div className='row'>
         <div className='col-2 pt-4 cat-image'>
-          <img src={catBg4} className="img-fluid " alt="cat-image" />
+          <img src={catBg4} className="img-fluid " alt="cat" />
         </div>
         <div className='question-title col-8 rounded-circle mt-3'>
-          <h1 className='text-center mt-4 fw-bolder'>Question 1</h1>
+          <h1 className='text-center mt-4 fw-bolder'>Question {questionIndex + 1}</h1>
         </div>
       </div>
 
-      <h2 className='fs-3 text-center my-4'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc id leo quis diam faucibus ornare. Morbi quis viverra nibh, in iaculis enim. Nulla facilisi?</h2>
+      <h2 className='fs-3 text-center my-4'>{question.question}</h2>
       <ul className="list-group answer-list text-center fw-medium fs-3">
-        <li className="list-group-item custom-list-item mb-4 rounded py-4">Proin metus urna</li>
-        <li className="list-group-item custom-list-item mb-4 rounded py-4">Proin metus urna</li>
-        <li className="list-group-item custom-list-item mb-4 rounded py-4">Proin metus urna</li>
-        <li className="list-group-item custom-list-item mb-4 rounded py-4">Proin metus urna</li>
+        <li onClick={(option) => { checkAnswer(option, 1) }} className="list-group-item custom-list-item mb-4 rounded py-4" ref={option1} >{question.option1}</li>
+        <li onClick={(option) => { checkAnswer(option, 2) }} className="list-group-item custom-list-item mb-4 rounded py-4" ref={option2} >{question.option2}</li>
+        <li onClick={(option) => { checkAnswer(option, 3) }} className="list-group-item custom-list-item mb-4 rounded py-4" ref={option3} >{question.option3}</li>
+        <li onClick={(option) => { checkAnswer(option, 4) }} className="list-group-item custom-list-item mb-4 rounded py-4" ref={option4} >{question.option4}</li>
       </ul>
 
 
       <button className='rounded mx-auto d-block btn btn-lg px-5 fw-semibold text-white mt-1'>Next Question</button>
-      <div className='text-center fs-5 '>1 of 5 question</div>
+      <div className='text-center fs-5 '>{questionIndex + 1} of {data.length} question{data.length > 1 ? 's' : ''}</div>
 
     </div>
   )
--- whisker-wisdom/src/Components/Quiz/Quiz.test.js
+++ whisker-wisdom/src/Components/Quiz/Quiz.test.js
@@ -1,3 +1,56 @@
 import React from "react";
-import { render } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import Quiz from "./Quiz";
+import { data } from '../../assets/JS/data';
+
+describe('Quiz Component', () => {
+    test('renders the first question and options', () => {
+        render(<Quiz />);
+
+        const questionElement = screen.getByText(data[0].question);
+        expect(questionElement).toBeInTheDocument();
+
+        const option1 = screen.getByText(data[0].option1);
+        expect(option1).toBeInTheDocument();
+
+        const option2 = screen.getByText(data[0].option2);
+        expect(option2).toBeInTheDocument();
+
+        const option3 = screen.getByText(data[0].option3);
+        expect(option3).toBeInTheDocument();
+
+        const option4 = screen.getByText(data[0].option4);
+        expect(option4).toBeInTheDocument();
+    });
+
+    test('checks if selecting the correct option applies correct class', () => {
+        render(<Quiz />);
+
+        const correctOptionIndex = data[0].ans - 1;
+
+        const option1 = screen.getByText(data[0].option1);
+        fireEvent.click(option1);
+
+        if (0 === correctOptionIndex) {
+            expect(option1).toHaveClass('bg-success');
+        } else {
+            expect(option1).toHaveClass('bg-danger');
+        }
+    });
+
+    test('checks if selecting an incorrect option applies the wrong class', () => {
+        render(<Quiz />);
+    
+        const incorrectOptionIndex = data[0].ans === 1 ? 2 : 1; // Choose an incorrect option index
+        const incorrectOptionText = data[0][`option${incorrectOptionIndex}`];
+        const incorrectOption = screen.getByText(incorrectOptionText);
+    
+        fireEvent.click(incorrectOption);
+    
+        expect(incorrectOption).toHaveClass('bg-danger');
+        const correctOptionText = data[0][`option${data[0].ans}`];
+        const correctOption = screen.getByText(correctOptionText);
+        expect(correctOption).toHaveClass('bg-success');
+    });
+    
+});
