085f0073720a82609316f0c487150605c17b13a9
Lauren Yeung <laurenyeung217@gmail.com>
2024-06-07 02:26:08 +0000

    Scores are displayed on the Fail.js and Pass.js pages.

--- whisker-wisdom/src/Components/Fail/Fail.js
+++ whisker-wisdom/src/Components/Fail/Fail.js
@@ -5,13 +5,16 @@ import './Fail.css'
 import catBg7 from '../../assets/images/cat7.png';
 
 const Fail = () => {
+    const location = useLocation();
+    const { score } = location.state || { score: 0 };
+
     return (
         <div>
             <div className="container landing-container mt-5">
-                <h1 className='display-5 text-center fw-bolder'>Good Job!</h1>
+                <h1 className='display-5 text-center fw-bolder'>Fur-get About It!</h1>
                 <img className='rounded mx-auto d-block img-fluid' src={catBg7} alt='' />
                 <div>
-                    <h3 className='fs-4 text-center'>Your marks is 5/10</h3>
+                    <h3 className='fs-4 text-center'>Your marks is {score}/10</h3>
                 </div>
                 <Link to="/quiz" className="text-decoration-none">
                     <button className='rounded mx-auto d-block btn btn-lg px-5 fw-semibold text-white my-4'>Take the quiz again</button>
--- whisker-wisdom/src/Components/Pass/Pass.js
+++ whisker-wisdom/src/Components/Pass/Pass.js
@@ -5,13 +5,17 @@ import './Pass.css'
 import catBg6 from '../../assets/images/cat6.png';
 
 const Pass = () => {
+
+  const location = useLocation();
+  const { score } = location.state || { score: 0 };
+
   return (
     <div>
       <div className="container landing-container mt-5">
         <h1 className='display-5 text-center fw-bolder'>Good Job!</h1>
         <img className='rounded mx-auto d-block img-fluid' src={catBg6} alt='' />
         <div>
-          <h3 className='fs-4 text-center'>Your marks is 10/10</h3>
+          <h3 className='fs-4 text-center'>Your marks is {score}/10</h3>
         </div>
         <Link to="/quiz" className="text-decoration-none">
           <button className='rounded mx-auto d-block btn btn-lg px-5 fw-semibold text-white my-4'>Take the quiz again</button>
