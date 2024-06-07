ab70e47b72fc0be11cddb00d270947b8150ac088
Lauren Yeung <laurenyeung217@gmail.com>
2024-06-07 02:41:33 +0000

    Added a new test to Pass.test.js

--- whisker-wisdom/src/Components/Pass/Pass.test.js
+++ whisker-wisdom/src/Components/Pass/Pass.test.js
@@ -1,3 +1,29 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import Result from './Pass';
+import { render, screen, fireEvent } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import Pass from './Pass';
+
+describe('Pass component', () => {
+  test('renders without crashing', () => {
+    render(
+      <MemoryRouter>
+        <Pass />
+      </MemoryRouter>
+    );
+    expect(screen.getByText('Good Job!')).toBeInTheDocument();
+  });
+
+  test('renders expected content', () => {
+    render(
+      <MemoryRouter>
+        <Pass />
+      </MemoryRouter>
+    );
+    expect(screen.getByText('Good Job!')).toBeInTheDocument();
+    expect(screen.getByText(/Your marks is/i)).toBeInTheDocument();
+    expect(screen.getByText('Take the quiz again')).toBeInTheDocument();
+    expect(screen.getByText('Back to home page')).toBeInTheDocument();
+  });
+
+
+});
