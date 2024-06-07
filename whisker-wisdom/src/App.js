5ca3c2d819e80f0761423beeb20da303c5af555c
Lauren Yeung <laurenyeung217@gmail.com>
2024-06-07 02:27:33 +0000

    Added routes for LandingPage, Intro, Quiz, Pass, and Fail components in the App.js file.

--- whisker-wisdom/src/App.js
+++ whisker-wisdom/src/App.js
@@ -1,15 +1,28 @@
 import React from 'react';
+import { Route, BrowserRouter as Router, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 import LandingPage from './Components/LandingPage/LandingPage';
 import Intro from './Components/Intro/Intro';
 import Quiz from './Components/Quiz/Quiz'
 import Pass from './Components/Pass/Pass';
 import Fail from './Components/Fail/Fail'
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/'>
+      <Route index element={<LandingPage />} />
+      <Route path='Intro' element={<Intro/>} />
+      <Route path='quiz' element={<Quiz/>} />
+      <Route path='pass' element={<Pass/>} />
+      <Route path='fail' element={<Fail/>} />
+    </Route>
+  )
+);
+
 const App = () => {
   return (
-    <>
-     < Fail />
-    </>
+    <RouterProvider router={router}>
+      <Router />
+    </RouterProvider>
   );
 }
 
